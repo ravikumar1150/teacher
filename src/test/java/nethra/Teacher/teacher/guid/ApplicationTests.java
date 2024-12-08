a10f0cb6e737c1af2173a8b0a8122c5059e40c96
ravi <ravikumark1150@gmail.com>
2024-12-08 09:40:36 +0530

    first commit

---
+++ src/test/java/nethra/Teacher/teacher/guid/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nethra.Teacher.teacher.guid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
